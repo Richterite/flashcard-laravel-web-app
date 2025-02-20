78bd857b0fa487b120097651933108ce91eb6237
Richterite <fikrifauzy156@gmail.com>
2025-02-20 12:06:07 +0700

    ✨ feat(page): add BaseInput and SubmitButton components for login form

---
+++ resources/js/Components/BaseInput.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {defineProps, defineEmits, ref} from 'vue'
+
+
+const props = withDefaults(
+    defineProps<{
+        label: string,
+        modelValue: string,
+        type?: string,
+        placeholder: string,
+        errorMessage: string,
+    }>(),
+    {
+        label: 'text',
+    },
+);
+
+const emit = defineEmits(["update:modelValue"])
+
+
+</script>
+
+<template>
+    <div class="mb-4">
+        <label class="block text-gray-600 mb-1">
+            {{ props.label }}
+        </label>
+        <input
+        :type="type"
+        :placeholder="placeholder"
+        :value="modelValue"
+        class="w-full p-3 border rounded-xl focus:ring-2 focus:ring-blue-400 focus:outline-none"
+        />
+        <p
+        v-if="errorMessage"
+        class="text-red-500 text-sm mt-1"
+        >
+            {{ props.errorMessage }}
+        </p>
+    </div>
+</template>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,10 +1,75 @@
 <script setup lang="ts">
+import BaseInput from '@/Components/BaseInput.vue';
+import SubmitButton from '@/Components/SubmitButton.vue';
+import { useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+const errors = ref<{
+    email?: string,
+    password?: string;
+}>({});
+
+const validateForm = () => {
+    errors.value = {};
+
+    if (!email.value) {
+        errors.value.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email.value)){
+        errors.value.email = 'Invalid Email format';
+    }
+
+    if (!password.value){
+        errors.value.password = 'Password is required';
+    }
+}
+
+const form = useForm({
+    email: '',
+    password: '',
+})
+
+
+const loginHandler = () => {
+    form
+    // validateForm();
+    console.log('Email:', email.value);
+    console.log('Password:', password.value);
+    alert("SUCCC")
+}
 
 
 </script>
 
 <template>
-    <div>
-        
+    <div class="flex min-h-screen items-center justify-center bg-gray-100">
+        <div class="w-full max-w-md bg-white p-6 rounded-2xl shadow-md">
+            <h2 class="text-2xl font-bold text-center mb-6 text-gray-700">
+                LOGIN
+            </h2>
+            <form @submit.prevent="loginHandler">
+                <BaseInput
+                label="Email"
+                v-model="email"
+                placeholder="Enter your email"
+                type="email"
+                :error-message="errors.email ? errors.email : ''"
+                />
+                <BaseInput
+                label="Password"
+                v-model="password"
+                placeholder="Enter your password"
+                type="password"
+                :error-message="errors.password ? errors.password : ''"
+                />
+
+                <SubmitButton 
+                button-label="Login"
+                type="submit"  
+                />
+
+            </form>
+        </div>
     </div>
 </template>
