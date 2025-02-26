ace167d9d0ba2bf50d414350694c4e6bbf7aca69
Richterite <fikrifauzy156@gmail.com>
2025-02-26 10:53:52 +0700

    🚚  refactor(component): move BaseButton  and BaseInput component into new component folder

---
+++ resources/js/components/BaseButton.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Loader2 } from 'lucide-vue-next';
+import { Button } from '@/components/ui/button';
+
+interface ButtonClass {
+  primary: string,
+  secondary: string,
+  danger: string,
+  outline: string, 
+  disabled: string, 
+}
+
+
+
+
+const props = withDefaults(defineProps<{
+    buttonLabel: string;
+    type: 'submit' | 'button' | 'reset';
+    variant?: InstanceType<typeof Button>["$props"]['variant'];
+    disabled?: boolean;
+}>(), {
+    buttonLabel: 'Submit',
+    type: 'submit',
+    variant: 'default',
+    disabled: false,
+})
+
+
+const isLoading = ref(false);
+</script>
+
+<template>
+    <div>
+        <Button
+        :type="props.type"
+        :disabled="isLoading"
+        class="w-full text-white p-3 rounded-xl transition flex items-center justify-center"
+        >
+        <Loader2 v-if="isLoading" class="w-4 h-4 mr-2 animate-spin"/>
+        
+          <span v-if="isLoading">Loading</span>
+          <span v-else>{{ props.buttonLabel }}</span>
+        </Button>
+        
+    </div>
+</template>
