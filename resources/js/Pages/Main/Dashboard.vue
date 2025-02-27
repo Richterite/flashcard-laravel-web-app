c61cb47cf1627dd1a26fd76748e325ae704a282f
Richterite <fikrifauzy156@gmail.com>
2025-02-27 10:53:24 +0700

    🚚 refactor(pages): move Dashboard.vue to Main/Dashboard.vue and update routing

---
+++ resources/js/Pages/Main/Dashboard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import ThemeSwtcher from '@/components/ThemeSwtcher.vue';
+import NavigationLayout from '@/Layouts/NavigationLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+
+    <Head title="Dashboard" />
+    <NavigationLayout>
+        <template #nav-right>
+            <ThemeSwtcher />
+        </template>
+        <template #nav-left>
+            <h1 class="text-2xl font-bold">
+                Flashcard
+            </h1>
+        </template>
+        <div class="main-page">
+            this is main page
+        </div>
+    </NavigationLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,9 @@ Route::get('/login', function () {
     return Inertia::render('Auth/Login');
 });
 
+
+Route::get('/', function () {
+    return Inertia::render('Main/Dashboard');
+});
+
 require __DIR__ . '/api.php';
