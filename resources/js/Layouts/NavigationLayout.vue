786391cb2b593793ea8f05a1faf25d701bc525df
Richterite <fikrifauzy156@gmail.com>
2025-02-27 10:54:54 +0700

    ✨ feat(layout): add NavigationLayout component for structured navigation and content display

---
+++ resources/js/Layouts/NavigationLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+        <div class="top-0 left-0 w-full p-4 border border-b-4">
+            <div class="flex justify-between items-center container mx-auto">
+                <div class="left-side">
+                    <slot name="nav-left"></slot>
+                </div>
+                <div class="right-side">
+                    <slot name="nav-right"></slot>
+                </div>
+            </div>
+        </div>
+    <div class="container mx-auto">
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
