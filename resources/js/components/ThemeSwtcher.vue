b922bfe535c7d2600675b7ffa0eab4d2bef68767
Richterite <fikrifauzy156@gmail.com>
2025-02-27 10:54:46 +0700

    ✨ feat(theme-switcher): add ThemeSwitcher component for toggling between light and dark modes

---
+++ resources/js/components/ThemeSwtcher.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core';
+import { Button } from './ui/button';
+import {Icon} from '@iconify/vue'
+
+
+const mode = useColorMode()
+const onClickHandler = () => {
+    mode.value = mode.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+    <Button variant="outline" @click="onClickHandler">
+        <Icon icon="radix-icons:moon" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Icon icon="radix-icons:sun" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span class="sr-only">
+            Toggle Theme
+        </span>
+    </Button>
+</template>
